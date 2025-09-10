8383a232d4d9694aff67755fc9b099e31a04b0ab
Roman L <romalaxov12@gmail.com>
2025-09-10 20:09:21 +0300

    Обновил код из-за не правильного вывода

--- greeting.py
+++ greeting.py
@@ -1,2 +1,6 @@
-name = input("Введите имя:")
-print(f"Привет, {name}!")
+def greet():
+    name = input("Введите ваше имя: ")
+    print(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    greet()
