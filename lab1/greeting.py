f2d1b3e60bd8e1ab2e9a60fa397a263ba03acfd6
Romkey-dev <romalaxov12@gmail.com>
2025-11-27 11:55:13 +0300

    Перенос файлов
    перенес все файлы в отдельную папку для удобства

---
+++ lab1/greeting.py
@@ -0,0 +1,6 @@
+def greet():
+    name = input("Введите ваше имя: ")
+    print(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    greet()
---
+++ lab1/hello.py
@@ -0,0 +1,2 @@
+print("axaxaxaxax)))")
+print("Precols))))")
